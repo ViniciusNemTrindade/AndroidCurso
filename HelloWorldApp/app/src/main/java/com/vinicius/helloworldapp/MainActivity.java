4e0c26b2f42d2afb475d9de3f6f0325ac56d96b5
Vinícius Trindade <viniciusnemtrindade@gmail.com>
2020-08-29 23:49:05 -0300

    Manipulação de atributos do tipo balão de texto ou pop-pup

--- HelloWorldApp/app/src/main/java/com/vinicius/helloworldapp/MainActivity.java
+++ HelloWorldApp/app/src/main/java/com/vinicius/helloworldapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,13 +16,17 @@ public class MainActivity extends AppCompatActivity {
 
         // int resultado = 5 * 3;
         // Log.i("Operacao: ", resultado + "");
-        // TextView texto = findViewById(R.id.texto);
+//         TextView texto = findViewById(R.id.texto);
         // Log.i("TAG", texto.getText().toString());
-//        EditText edtNome = findViewById(R.id.editeTexto);
+        EditText edtNome = findViewById(R.id.editeTexto);
 //        Log.i("NOME: ", edtNome.getText().toString());
 
         EditText edtNumTelefone = findViewById(R.id.editeTelefone);
-        Log.i("NÚMERO: ", edtNumTelefone.getText().toString());
+//        Log.i("NÚMERO: ", edtNumTelefone.getText().toString());
+//        Toast.makeText(MainActivity.this, texto.getText().toString(), Toast.LENGTH_LONG).show();
+
+        Toast.makeText(MainActivity.this, "Seu nome eh: "+ edtNome.getText().toString() + "\n"
+                + "Seu telefone eh: " + edtNumTelefone.getText().toString(), Toast.LENGTH_LONG).show();
 
     }
 
