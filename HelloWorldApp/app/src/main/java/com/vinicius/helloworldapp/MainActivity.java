ed9c8043da0092ccc43df480a6a25b28f7d0cc2a
Vinícius Trindade <viniciusnemtrindade@gmail.com>
2020-08-29 21:03:06 -0300

    Propriedades de botões: margin, color, background, padding e evento onClick

--- HelloWorldApp/app/src/main/java/com/vinicius/helloworldapp/MainActivity.java
+++ HelloWorldApp/app/src/main/java/com/vinicius/helloworldapp/MainActivity.java
@@ -3,8 +3,20 @@ package com.vinicius.helloworldapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public void btnPressionar(View buttonView ) {
+
+        // int resultado = 5 * 3;
+        // Log.i("Operacao: ", resultado + "");
+        TextView texto = findViewById(R.id.texto);
+        Log.i("TAG", texto.getText().toString());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
