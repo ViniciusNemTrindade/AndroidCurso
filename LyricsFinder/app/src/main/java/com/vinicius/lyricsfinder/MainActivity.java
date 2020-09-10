b15e1d9b1cf6d0e3224d33cd932cb1048150a209
Vinícius Trindade <viniciusnemtrindade@gmail.com>
2020-09-10 06:57:30 -0300

    App LyricsFinder finalizado - na sua criação foram abordados parcialmente conceitos e práticas utilizando o Json para incorporar ao app uma api que disponibiliza Letras de músicas

--- LyricsFinder/app/src/main/java/com/vinicius/lyricsfinder/MainActivity.java
+++ LyricsFinder/app/src/main/java/com/vinicius/lyricsfinder/MainActivity.java
@@ -2,6 +2,8 @@ package com.vinicius.lyricsfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
+import android.net.UrlQuerySanitizer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,13 +11,23 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText editeNomeArtista, editeNomeMusica;
     Button btnBuscaLetrasMusica;
     TextView textoLetras;
-
-    //https://api.lyrics.ovh/v1/Rihanna/Diamonds#
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +42,27 @@ public class MainActivity extends AppCompatActivity {
         btnBuscaLetrasMusica.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Este botão foi pressionado!", Toast.LENGTH_SHORT).show();
+               // Toast.makeText(getApplicationContext(), "Este botão foi pressionado!", Toast.LENGTH_SHORT).show();
+                String url = "https://api.lyrics.ovh/v1/" + editeNomeArtista.getText().toString() + "/" + editeNomeMusica.getText().toString();
+                url.replace(" ", "%20");
+                RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                textoLetras.setText(response.getString("lyrics"));
+                            } catch(JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        });
+                requestQueue.add(jsonObjectRequest);
             }
         });
     }
