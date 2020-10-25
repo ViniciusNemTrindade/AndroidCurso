a6bcbcf0f39399b09290162aff2053433e0c5253
Vinícius Trindade <viniciusnemtrindade@gmail.com>
2020-10-25 02:39:00 -0300

    criando barra de volume para aplicação - utilizando SeekBar

--- AudioeVideo/app/src/main/java/com/vinicius/audioevideo/MainActivity.java
+++ AudioeVideo/app/src/main/java/com/vinicius/audioevideo/MainActivity.java
@@ -2,22 +2,30 @@ package com.vinicius.audioevideo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
+import android.widget.SeekBar;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    // Ui Components
     private VideoView myVideoView;
     private Button btnPlayVideo;
     private MediaController mediaController;
     private Button btnPlayMusic;
     private Button btnPauseMusic;
+
     private MediaPlayer mediaPlayer;
+
+    private SeekBar volumeSeekBar;
+    private AudioManager audioManager;
+
 
 
     @Override
@@ -29,39 +37,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnPlayVideo = findViewById(R.id.btnPlay);
         btnPlayMusic = findViewById(R.id.btnMusic);
         btnPauseMusic = findViewById(R.id.btnPause);
+        volumeSeekBar = findViewById(R.id.seekBarVolume);
 
         btnPlayVideo.setOnClickListener(MainActivity.this);
         btnPlayMusic.setOnClickListener(MainActivity.this);
         btnPauseMusic.setOnClickListener(MainActivity.this);
 
         mediaController = new MediaController(MainActivity.this);
-        mediaPlayer = new MediaPlayer.create(this, R.raw.dragon_ball);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.tobu_mesmerize);
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+
+        int maximoVolumedoDispositivoDoUsuario = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int volumeAtualDoDispositivoDoUsuario = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        volumeSeekBar.setMax(maximoVolumedoDispositivoDoUsuario);
+        volumeSeekBar.setProgress(volumeAtualDoDispositivoDoUsuario);
+
+        volumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
     }
 
     @Override
     public void onClick(View buttonView) {
 
-        switch (buttonView) {
+        switch (buttonView.getId()) {
             case R.id.btnPlay:
                 Uri videoUri = Uri.parse("android.resource://" +  getPackageName() + "/" + R.raw.airbaloon);
                 myVideoView.setVideoURI(videoUri);
-
                 myVideoView.setMediaController(mediaController);
                 mediaController.setAnchorView(myVideoView );
-
                 myVideoView.start();
                 break;
+
             case R.id.btnMusic:
-                Uri videoUri = Uri.parse("android.resource://" +  getPackageName() + "/" + R.raw.airbaloon);
-                myVideoView.setVideoURI(videoUri);
-
-                myVideoView.setMediaController(mediaController);
-                mediaController.setAnchorView(myVideoView );
-
-                myVideoView.start();
+                mediaPlayer.start();
                 break;
-            case R.id.btnPause
+
+            case R.id.btnPause:
+                mediaPlayer.pause();
                 break;
 
         }
