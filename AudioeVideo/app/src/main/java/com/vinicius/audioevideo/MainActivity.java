9f7d175910c177e91c6379dc6e2b811862dadff5
Vinícius Trindade <viniciusnemtrindade@gmail.com>
2020-10-25 00:00:09 -0300

    criando videos e manipulando suas ferramentas

--- AudioeVideo/app/src/main/java/com/vinicius/audioevideo/MainActivity.java
+++ AudioeVideo/app/src/main/java/com/vinicius/audioevideo/MainActivity.java
@@ -2,15 +2,22 @@ package com.vinicius.audioevideo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity implements View.OnclickL{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private VideoView myVideoView;
     private Button btnPlayVideo;
+    private MediaController mediaController;
+    private Button btnPlayMusic;
+    private Button btnPauseMusic;
+    private MediaPlayer mediaPlayer;
 
 
     @Override
@@ -19,6 +26,46 @@ public class MainActivity extends AppCompatActivity implements View.OnclickL{
         setContentView(R.layout.activity_main);
 
         myVideoView = findViewById(R.id.videoView);
-        btnPlayVideo = findViewById(R.id.btnPlayId);
+        btnPlayVideo = findViewById(R.id.btnPlay);
+        btnPlayMusic = findViewById(R.id.btnMusic);
+        btnPauseMusic = findViewById(R.id.btnPause);
+
+        btnPlayVideo.setOnClickListener(MainActivity.this);
+        btnPlayMusic.setOnClickListener(MainActivity.this);
+        btnPauseMusic.setOnClickListener(MainActivity.this);
+
+        mediaController = new MediaController(MainActivity.this);
+        mediaPlayer = new MediaPlayer.create(this, R.raw.dragon_ball);
+
+    }
+
+    @Override
+    public void onClick(View buttonView) {
+
+        switch (buttonView) {
+            case R.id.btnPlay:
+                Uri videoUri = Uri.parse("android.resource://" +  getPackageName() + "/" + R.raw.airbaloon);
+                myVideoView.setVideoURI(videoUri);
+
+                myVideoView.setMediaController(mediaController);
+                mediaController.setAnchorView(myVideoView );
+
+                myVideoView.start();
+                break;
+            case R.id.btnMusic:
+                Uri videoUri = Uri.parse("android.resource://" +  getPackageName() + "/" + R.raw.airbaloon);
+                myVideoView.setVideoURI(videoUri);
+
+                myVideoView.setMediaController(mediaController);
+                mediaController.setAnchorView(myVideoView );
+
+                myVideoView.start();
+                break;
+            case R.id.btnPause
+                break;
+
+        }
+
+
     }
 }
