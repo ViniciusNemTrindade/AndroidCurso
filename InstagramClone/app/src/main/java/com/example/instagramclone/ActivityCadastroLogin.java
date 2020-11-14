943c48e6a3c74675dbcf7a29866c4fd253e85ff3
ViniciusNemTrindade <viniciusnemtrindade@gmail.com>
2020-11-14 06:14:19 -0300

    Conectando no ActivityCadastroLogin.java ao Servidor com ParserObject e ParseUser

--- InstagramClone/app/src/main/java/com/example/instagramclone/ActivityCadastroLogin.java
+++ InstagramClone/app/src/main/java/com/example/instagramclone/ActivityCadastroLogin.java
@@ -1,17 +1,51 @@
 package com.example.instagramclone;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.parse.ParseUser;
+
 public class ActivityCadastroLogin extends AppCompatActivity {
+
+    private EditText mEdtCadastroNomeUsuario;
+    private EditText mEdtCadastroSenhaUsuario;
+    private Button mBtnCadastroUsuario;
+
+    private EditText mEdtLoginNomeUsuario;
+    private EditText mEdtLoginSenhaUsuario;
+    private Button mBtnLoginUsuario;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_login);
 
+        mEdtCadastroNomeUsuario = findViewById(R.id.edtCadastroNomeUsuario);
+        mEdtCadastroSenhaUsuario = findViewById(R.id.edtCadastroSenhaUsuario);
+        mBtnCadastroUsuario = findViewById(R.id.btnCadastroUsuario);
+        mBtnCadastroUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                ParseUser appUser = new ParseUser();
+                appUser.setUsername(mEdtCadastroNomeUsuario.getText().toString());
+                appUser.setPassword(mEdtCadastroNomeUsuario.getText().toString());
+            }
+        });
+
+        mEdtLoginNomeUsuario = findViewById(R.id.edtLoginNomeUsuario);
+        mEdtLoginSenhaUsuario = findViewById(R.id.edtLoginSenhaUsuario);
+        mBtnLoginUsuario = findViewById(R.id.btnLogarUsuario);
+        mBtnLoginUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
